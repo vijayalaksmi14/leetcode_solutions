5cca15e0d098b9cc0f969dae8698d7cb29e923f2
Gummalla vijayalakshmi <gummallavijayalashmi84@gmail.com>
2026-09-07 12:16:26 +0530

    Add hand-written paper design PDF

--- input.js
+++
@@ -1,6 +0,0 @@
-function calculateTotal(price,tax){
-const total = price + (price * tax)
-  return total ;
-}
-const user={name:"Alice",age:30,hobbies:["reading",'coding',]}
-console.log(  calculateTotal( 100 , 0.08 ) )
